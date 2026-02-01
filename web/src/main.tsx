297d8b8742d28d9f1ef5b21acd16d0efe818a805
rakshapahariya <rakshapahariya1208@gmail.com>
2026-02-02 00:22:01 +0530

    Debugging Supa Varibales

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import AppShell from "./shell/appShell";
-import "./app.css";
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
